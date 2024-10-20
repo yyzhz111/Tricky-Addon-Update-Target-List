21b3c9a3bc6f9bfd3114e0ae4c4ca249367249aa
KOWX712 <leecc0503@gmail.com>
2024-10-20 21:43:20 +0800

    update service.sh

--- module/service.sh
+++ module/service.sh
@@ -1,18 +1,15 @@
 MODDIR=${0%/*}
 COMPATH="$MODDIR/common"
-# Check pm command functionality
-sleep 10
-until pm list packages > /dev/null 2>&1; do
-    sleep 1
-done
-
-# Check tricky store status
 TS="/data/adb/modules/tricky_store"
-if [ -f "$TS/disable" ]; then
-    cat "$COMPATH/disabled" > "$MODDIR/module.prop"
-elif  [ ! -d "$TS" ]; then
+
+if [ ! -d "$TS" ]; then
     cat "$COMPATH/ninstalled" > "$MODDIR/module.prop"
+elif  [ -f "$TS/disable" ]; then
+    cat "$COMPATH/disabled" > "$MODDIR/module.prop"
 else
     cat "$COMPATH/normal" > "$MODDIR/module.prop"
-    sh "$MODDIR"/common/UpdateTargetList.sh || true
+    until [ "$(getprop sys.boot_completed)" = "1" ]; do
+        sleep 1
+    done
+    . "$COMPATH/common/UpdateTargetList.sh"
 fi
