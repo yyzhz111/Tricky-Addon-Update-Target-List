151e92aa19d1e7ff17213af9700c73275b983c04
KOWX712 <leecc0503@gmail.com>
2024-10-20 21:47:50 +0800

    update Tricky Addon Lite

--- lite-script_only/UpdateTargetList.sh
+++ lite-script_only/UpdateTargetList.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
 # by KOW
-# v1.2
+# Tricky Addon Lite: Update Target List Script v1.3
 # GitHub release: https://github.com/KOWX712/Tricky-Addon-Update-Target-List/releases/latest
 # telegram channel: https://t.me/kowchannel
 
 # This script will put all non-system app into /data/adb/tricky_store/target.txt
-# Using module to put normal app into system app may exclude corresponding app from this script too, please disable it if you found this script doesn't work.
+
+# Configurable exclude and addition list
+# DO NOT remove default package names here
+EXCLUDE="
+oneplus
+coloros
+com.android.patch
+me.bmax.apatch"
+
+ADDITION="
+com.google.android.gms
+io.github.vvb2060.keyattestation
+io.github.vvb2060.mahoshojo
+icu.nullptr.nativetest"
 
 echo " "
 echo " Staring script..."
@@ -18,13 +31,14 @@ su -c > /data/adb/tricky_store/target.txt
 echo " Adding apps to target.txt..."
 echo " "
 
-EXCLUDE="oneplus|coloros|com.android.patch|me.bmax.apatch"
+EXCLUDE=$(echo "$EXCLUDE" | tr '\n' '|' | sed 's/^|//;s/|$//')
+ADDITION=$(echo "$ADDITION" | tr '\n' ' ' | sed 's/^ //;s/ $//')
+
 # Add all non-system apps to the target file and remove exclusions
 su -c pm list packages -3 </dev/null 2>&1 | cat | awk -F: '{print $2}' | grep -Ev "$EXCLUDE" > /data/adb/tricky_store/target.txt
 sleep 1
 
 # Add extra package names if any app excluded by the script (DO NOT remove default package here)
-ADDITION="com.google.android.gms io.github.vvb2060.keyattestation io.github.vvb2060.mahoshojo icu.nullptr.nativetest"
 for app in $ADDITION; do
     if ! grep -qx "$app" /data/adb/tricky_store/target.txt; then
         echo "$app" >> /data/adb/tricky_store/target.txt
