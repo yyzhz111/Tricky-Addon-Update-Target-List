e9346303ac379984b6118d1d04736b9e0ef66a73
KOWX712 <leecc0503@gmail.com>
2024-11-14 21:37:32 +0800

    prevent tspa overwrite

--- module/service.sh
+++ module/service.sh
@@ -1,10 +1,18 @@
 MODPATH=${0%/*}
 TS="/data/adb/modules/tricky_store"
 SCRIPT_DIR="/data/adb/tricky_store"
+TSPA="/data/adb/modules/tsupport-advance"
 
 hash_value=$(grep -v '^#' "$MODPATH/boot_hash" | tr -d '[:space:]')
 if [ -n "$hash_value" ]; then
     resetprop -n ro.boot.vbmeta.digest "$hash_value"
+fi
+
+# Disable TSupport-A auto update target to prevent overwrite
+if [ -d "$TSPA" ]; then
+    touch "/storage/emulated/0/stop-tspa-auto-target"
+elif [ ! -d "$TSPA" ] && [ -f "/storage/emulated/0/stop-tspa-auto-target" ]; then
+    rm -f "/storage/emulated/0/stop-tspa-auto-target"
 fi
 
 if [ ! -f "$MODPATH/common/module.prop.orig" ]; then
@@ -13,6 +21,7 @@ if [ ! -f "$MODPATH/common/module.prop.orig" ]; then
     exit 1
 fi
 
+# Check availability of Tricky Store module
 if [ ! -d "$TS" ]; then
     sed -i 's/^description=.*/description=Tricky store is not installed/' "$MODPATH/module.prop"
     touch "$MODPATH/disable"
--- module/uninstall.sh
+++ module/uninstall.sh
@@ -1,6 +1,11 @@
 MODPATH=${0%/*}
 SCRIPT_DIR="/data/adb/tricky_store"
 
+# Enable back TSupport-A auto update
+if [ -f "/storage/emulated/0/stop-tspa-auto-target" ]; then
+    rm -f "/storage/emulated/0/stop-tspa-auto-target"
+fi
+
 # Remove residue and restore original keybox.
 rm -rf "$SCRIPT_DIR/target_list_config"
 rm -f "$SCRIPT_DIR/UpdateTargetList.sh"
