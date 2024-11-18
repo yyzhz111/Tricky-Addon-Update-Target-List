6f7108543f50cef65741420a038088a4bc03caaa
KOWX712 <leecc0503@gmail.com>
2024-11-18 20:33:37 +0800

    cleanup residue
    cleanup residue during remove module

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -9,6 +9,7 @@ fi
 # Remove residue and restore original keybox.
 rm -rf "$SCRIPT_DIR/target_list_config"
 rm -f "$SCRIPT_DIR/UpdateTargetList.sh"
+rm -f "/data/adb/boot_hash"
 if [ -f "$MODPATH/common/origkeybox" ]; then
     rm -f "$SCRIPT_DIR/keybox.xml"
     mv "$MODPATH/common/origkeybox" "$SCRIPT_DIR/keybox.xml"
