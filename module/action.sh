a399c4a69a9b731a994898d1aa1cc4b3fab768a9
KOWX712 <leecc0503@gmail.com>
2024-12-01 22:57:34 +0800

    action: abandon UpdateTargetList.sh

--- module/action.sh
+++ module/action.sh
@@ -12,26 +12,6 @@ if pm list packages | grep -q "$PACKAGE_NAME"; then
     echo "- Launching KSU WebUI..."
     am start -n "${PACKAGE_NAME}/.WebUIActivity" -e id "$MODID"
 else
-    SKIP_FILE="$SCRIPT_DIR/target_list_config/skipwebui"
-    if [ ! -f "$SKIP_FILE" ]; then
-        echo "**********************************************"
-        echo "- Do you want to install KSU WebUI standalone?"
-        echo "  VOL [+]: YES"
-        echo "  VOL [-]: NO"
-        echo "**********************************************"
-
-        key_check
-        if [[ "$keycheck" == "KEY_VOLUMEUP" ]]; then
-            echo "- Installing KSU WebUI..."
-            . "$COMPATH/get_WebUI.sh"
-        else
-            echo "- Skipping WebUI installation..."
-            touch "$SKIP_FILE"
-            echo "- Skip WebUI check until next installation."
-            echo ""
-            update_script
-        fi
-    else
-        update_script
-    fi
+    echo "- Installing KSU WebUI..."
+    . "$COMPATH/get_WebUI.sh"
 fi
