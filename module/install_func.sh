18e30480287c01eedc410363570985a1efb05f81
KOWX712 <leecc0503@gmail.com>
2024-12-01 22:57:22 +0800

    installation: abandon UpdateTargetList.sh

--- module/install_func.sh
+++ module/install_func.sh
@@ -2,17 +2,12 @@ initialize() {
     if [ -f "$SCRIPT_DIR/UpdateTargetList.sh" ]; then
         rm -f "$SCRIPT_DIR/UpdateTargetList.sh"
     fi
-    if [ -f "$CONFIG_DIR/skipwebui" ]; then
-        rm -f "$CONFIG_DIR/skipwebui"
-    fi
     if [ -d "/data/adb/modules/$NEW_MODID" ]; then
         rm -rf "/data/adb/modules/$NEW_MODID"
     fi
     
     cp "$MODPATH/module.prop" "$COMPATH/temp/module.prop"
     cp "$COMPATH/.default" "$COMPATH/temp/.default"
-    mv "$COMPATH/UpdateTargetList.sh" "$SCRIPT_DIR/UpdateTargetList.sh"
-    set_perm $SCRIPT_DIR/UpdateTargetList.sh 0 2000 0755
     set_perm $COMPATH/get_extra.sh 0 2000 0755
     set_perm $COMPATH/get_WebUI.sh 0 2000 0755
     
@@ -46,47 +41,9 @@ initialize() {
     rm -rf "$MODPATH/bin"
 }
 
-add_exclude() {
-    EXCLUDE=$(grep -vE '^[[:space:]]*#|^[[:space:]]*$' "$CONFIG_DIR/EXCLUDE")
-    for app in $EXCLUDE; do
-        app=$(echo "$app" | tr -d '[:space:]')
-        if ! grep -Fq "$app" $COMPATH/EXCLUDE; then
-            echo "$app" >> $COMPATH/EXCLUDE
-        fi
-    done
-    mv "$COMPATH/EXCLUDE" "$CONFIG_DIR/EXCLUDE"
-}
-
-add_addition() {
-    ADDITION=$(grep -vE '^[[:space:]]*#|^[[:space:]]*$' "$CONFIG_DIR/ADDITION")
-    for app in $ADDITION; do
-        app=$(echo "$app" | tr -d '[:space:]')
-        if ! grep -Fq "$app" $COMPATH/ADDITION; then
-            echo "$app" >> $COMPATH/ADDITION
-        fi
-    done
-    mv "$COMPATH/ADDITION" "$CONFIG_DIR/ADDITION"
-}
-
 find_config() {
     if [ -d "$CONFIG_DIR" ]; then
-        if [ ! -f "$CONFIG_DIR/EXCLUDE" ] && [ ! -f "$CONFIG_DIR/ADDITION" ]; then
-            mv "$COMPATH/EXCLUDE" "$CONFIG_DIR/EXCLUDE"
-            mv "$COMPATH/ADDITION" "$CONFIG_DIR/ADDITION"
-        elif [ ! -f "$CONFIG_DIR/ADDITION" ]; then
-            mv "$COMPATH/ADDITION" "$CONFIG_DIR/ADDITION"
-            add_exclude
-        elif [ ! -f "$CONFIG_DIR/EXCLUDE" ]; then
-            mv "$COMPATH/EXCLUDE" "$CONFIG_DIR/EXCLUDE"
-            add_addition
-        else
-            add_exclude
-            add_addition
-        fi
-    else
-        mkdir -p "$CONFIG_DIR"
-        mv "$COMPATH/EXCLUDE" "$CONFIG_DIR/EXCLUDE"
-        mv "$COMPATH/ADDITION" "$CONFIG_DIR/ADDITION"
+        rm -rf "$CONFIG_DIR"
     fi
 }
 
