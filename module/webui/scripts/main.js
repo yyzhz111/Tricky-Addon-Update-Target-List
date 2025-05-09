0ecd59414ef19b9e2093754e491d70f967955bb9
KOWX712 <leecc0503@gmail.com>
2025-05-09 18:07:29 +0800

    feat: add create home screen shortcut option

--- module/webui/scripts/main.js
+++ module/webui/scripts/main.js
@@ -192,6 +192,14 @@ function checkMMRL() {
     if (window.$tricky_store && Object.keys($tricky_store).length > 0) {
         // Set status bars theme based on device theme
         $tricky_store.setLightStatusBars(!window.matchMedia('(prefers-color-scheme: dark)').matches)
+
+        // Create home screen shortcut
+        const shortcutButton = document.getElementById('shortcut');
+        shortcutButton.style.display = 'flex';
+        shortcutButton.addEventListener('click', () => {
+            $tricky_store.createShortcut();
+            showPrompt("prompt.shortcut_created", true);
+        });
     }
 }
 
